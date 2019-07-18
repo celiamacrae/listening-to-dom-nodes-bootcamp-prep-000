852d2338e722026dfff52960404e49394ee1b6fc
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-18 18:30:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -52,3 +52,30 @@ for (let i = 0; i < divs.length; i++) {
 
 
 
+
+
+///capturing 
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
+
+
+///RELOAD PAGE 
+/// StopPropogation 
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
