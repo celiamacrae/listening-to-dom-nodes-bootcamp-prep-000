36fe15e0def97569fb0dbe91e06f3d54a56f0782
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-18 18:27:59 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+// prints out keystrokes into console
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
+});
